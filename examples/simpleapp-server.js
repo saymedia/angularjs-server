bfd6a0605a05410647573eac53849f2302699e17
Martin Atkins <matkins@saymedia.com>
2013-10-30 17:18:18 -0700

    Shim the rendered page so we can still run AngularJS in the browser.
    Previously we just ran AngularJS on the server and served completely static
    HTML to the browser. This was fine as a prototype of running Angular
    on the server, but in real applications we still want to get the full
    benefits of AngularJS on the client too, so to move closer to that goal
    this shims the static HTML so that, if rendered in a browser that
    supports JavaScript, we'll replace the DOM with the template and then load
    some other scripts to complete the client-side bootstrap.
    
    At the moment this is only partially implemented in our simple example,
    with the shim in place but without a client-side bootstrapping script.
    This means that right now the example is actually broken since it'll
    just render the template on-screen and never bootstrap in the client.

--- examples/simpleapp-server.js
+++ examples/simpleapp-server.js
@@ -8,15 +8,21 @@ var templateFile = path.join(__dirname, 'simpleapp.html');
 var template = fs.readFileSync(templateFile, {encoding:'utf8'});
 
 var server = angularserver.createServer(
-    template,
-    [
-        path.join(__dirname, 'angular.js'),
-        path.join(__dirname, 'angular-route.js'),
-        path.join(__dirname, 'simpleapp.js')
-    ],
-    [
-        'simpleapp'
-    ]
+    {
+        template: template,
+        serverScripts: [
+            path.join(__dirname, 'angular.js'),
+            path.join(__dirname, 'angular-route.js'),
+            path.join(__dirname, 'simpleapp.js')
+        ],
+        staticResourceDir: __dirname,
+        clientScripts: [
+            'baz.js'
+        ],
+        angularModules: [
+            'simpleapp'
+        ]
+    }
 );
 
 server.listen(9008);
