daef7647c04a7dc3324cde119b257f2845008525
Martin Atkins <matkins@saymedia.com>
2013-10-31 17:51:30 -0700

    Include the initial route in the HTML response.
    Currently this doesn't actually do anything, but it's a step towards
    having the router in the client know exactly what to render without having
    to do any further requests.

--- examples/simpleapp.js
+++ examples/simpleapp.js
@@ -19,11 +19,11 @@ app.config(
                     weatherData: function ($http) {
                         return $http.get(
                             'http://api.openweathermap.org/data/2.5/weather?q=San%20Francisco,%20CA,%20US'
-                        );
+                        ).then(function (resp) { return resp.data; });
                     }
                 },
                 controller: function (weatherData, $scope) {
-                    $scope.weather = weatherData.data;
+                    $scope.weather = weatherData;
                 }
             }
         );
